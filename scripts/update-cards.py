f049f7db53400a2f6963a7673c6965463e9e278a
rick.stokkingreef <rick.stokkingreef@takeaway.com>
2022-04-01 12:13:45 +0200

    Update lovelace-card-mod to fix april foolsday joke

--- scripts/update-cards.py
+++ scripts/update-cards.py
@@ -6,8 +6,8 @@ cards = (
     ('button-card', 'https://github.com/custom-cards/button-card/releases/download/3.4.1/button-card.js'),
     ('light-entity-card', 'https://github.com/ljmerza/light-entity-card/releases/download/4.5.0/light-entity-card.js'),
     ('lovelace-auto-entities', 'https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/1.11.0/auto-entities.js'),
-    # card-mod: https://github.com/thomasloven/lovelace-card-mod/tree/3.1.2#performance-improvements
-    ('lovelace-card-mod', 'https://raw.githubusercontent.com/thomasloven/lovelace-card-mod/3.1.2/card-mod.js'),
+    # card-mod: https://github.com/thomasloven/lovelace-card-mod/tree/3.1.4#performance-improvements
+    ('lovelace-card-mod', 'https://raw.githubusercontent.com/thomasloven/lovelace-card-mod/3.1.4/card-mod.js'),
     ('mini-graph-card', 'https://github.com/kalkih/mini-graph-card/releases/download/v0.11.0-dev.5/mini-graph-card-bundle.js'),
     ('mini-media-player', 'https://github.com/kalkih/mini-media-player/releases/download/v1.16.2/mini-media-player-bundle.js'),
     ('my-cards-slider-card', 'https://github.com/AnthonMS/my-cards/blob/main/dist/my-cards.js'),
